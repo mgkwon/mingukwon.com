d1f46ac5ae7ccbdb7fd19d7ea29a5aefdf0b270f
Mingu Kwon <mgskwon@gmail.com>
2019-01-03 23:45:01 +0800

    added navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+import './navigation/Navigation.jsx';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +21,9 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
+        <div className="App-nav" />
+        <div className="App-front-page" />
       </div>
     );
   }
